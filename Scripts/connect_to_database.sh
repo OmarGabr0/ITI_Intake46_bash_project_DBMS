f85844f119d74b5a0cc03702815de071c550daf8
OmarGabr0 <omargabr90@gmail.com>
2025-12-30 18:42:05 +0200

    updating gui.sh and connect_to_database.sh  to work with zenti

--- Scripts/connect_to_database.sh
+++ Scripts/connect_to_database.sh
@@ -1,111 +1,86 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
 declare -a buffer
-
-# Sources section
 source repeating_functions.sh
 
-if [ $# -eq 0 ]
-then
-	while true
-	do
-		read -p "Enter the database name : " DB_Name
-		is_empty $DB_Name
-		if [ $? -eq 0 ]
-			then 
-			DB_Path="../Databases/$DB_Name"
-			break
-		else 
-			echo "System doesn't accept empty inputs"
-		fi 
-	done
-
-elif [ $# -eq 1 ]
-then
-	DB_Name="$1"
-	DB_Path="../Databases/$DB_Name"
+if [ $# -eq 0 ]; then
+    while true; do
+        DB_Name=$(zenity --entry --title="Connect to Database" --text="Enter the database name:")
+        is_empty $DB_Name
+        if [ $? -eq 0 ]; then
+            DB_Path="../Databases/$DB_Name"
+            break
+        else
+            zenity --error --text="Empty input is not allowed!"
+        fi
+    done
+elif [ $# -eq 1 ]; then
+    DB_Name="$1"
+    DB_Path="../Databases/$DB_Name"
 fi
 
-while true
-do
-		
-		check_if_exists d $DB_Path
-		if [ $? -eq 1 ]
-		then 
-		# While loop for the select to display the menu after choosing any option
-		while true
-		do
-			 PS3="Enter a number : "
-			select choice in "Create Table" "List Tables" "Drop Table" "Insert into Table" "Select from Table" "Delete From Table" "Update Table" "Exit" 
-			do
-				case $REPLY in
-				1)
-				while true
-				do
-				 read -p "Enter table name : " table_name
-				 is_empty $table_name
-				 if [ $? -eq 1 ]
-				 then echo "System doesn't accept empty inputs"
-				 else break
-				 fi
-				 done
-				 source create_table.sh $DB_Name $table_name
-					break;;
-				2) ls $DB_Path 
-					break;;
-				3)  
-					read -p "Enter table name : " table_name
-					source drop_table.sh $DB_Name $table_name
-					break;;
-				4) source insert_into_table.sh $DB_Name
-					break;;
-				5) source enhanced_select_from_table.sh $DB_Name
+while true; do
+    check_if_exists d $DB_Path
+    if [ $? -eq 1 ]; then
+        while true; do
+            choice=$(zenity --list --title="Database: $DB_Name" \
+                --column="Option" \
+                "Create Table" \
+                "List Tables" \
+                "Drop Table" \
+                "Insert into Table" \
+                "Select from Table" \
+                "Delete From Table" \
+                "Update Table" \
+                "Exit")
 
-					break;;
-				6) source delete_from_table.sh $DB_Name
-					break;;
-				7) source update_table.sh $DB_Name
-					break;;
-				8) source main.sh
-				;;
-				*) echo "Invalid option"
-					break;;
-				esac
-			done
-			# Wait a second until display is read
-		      sleep 0.5	
-		done
-		else
-			echo "Database name you entered is not found"
-			select choice in "Enter another name" "Exit from this menu"
-			do 
-				case $REPLY in
-				1) 
-					while true
-					do
-						read -p "Enter a valid name: " DB_Name
-						is_empty $DB_Name
-						if [ $? -eq 0 ]
-						then
-							DB_Path="../Databases/$DB_Name"
-							break
-						else
-							echo "Empty inputs are not allowed" 
-						fi
-					done
-				break
-				;;
-				
-				2) 
-				break 2
-				;;
-				
-				*) 
-				echo "Invalid option"
-				;;
-				esac
-			done	
-		fi
+            case $choice in
+                "Create Table")
+                    table_name=$(zenity --entry --title="Create Table" --text="Enter table name:")
+                    if [ -n "$table_name" ]; then
+                        ./create_table.sh "$DB_Name" "$table_name"
+                        zenity --info --text="Table '$table_name' created successfully!"
+                    fi
+                    ;;
+                "List Tables")
+                    tables=$(ls $DB_Path | tr '\n' ' ')
+                    zenity --info --title="Tables in $DB_Name" --text="$tables"
+                    ;;
+                "Drop Table")
+                    table_name=$(zenity --entry --title="Drop Table" --text="Enter table name to drop:")
+                    if [ -n "$table_name" ]; then
+                        ./drop_table.sh "$DB_Name" "$table_name"
+                        zenity --info --text="Table '$table_name' dropped!"
+                    fi
+                    ;;
+                "Insert into Table")
+                    ./insert_into_table.sh "$DB_Name"
+                    ;;
+                "Select from Table")
+                    ./enhanced_select_from_table.sh "$DB_Name"
+                    ;;
+                "Delete From Table")
+                    ./delete_from_table.sh "$DB_Name"
+                    ;;
+                "Update Table")
+                    ./update_table.sh "$DB_Name"
+                    ;;
+                "Exit")
+                    exit 0
+                    ;;
+                *)
+                    zenity --error --text="Invalid option!"
+                    ;;
+            esac
+        done
+    else
+        retry=$(zenity --list --title="Database Not Found" \
+            --column="Option" "Enter another name" "Exit")
+        case $retry in
+            "Enter another name")
+                ;;
+            "Exit")
+                exit 0
+                ;;
+        esac
+    fi
 done
-
-
-
